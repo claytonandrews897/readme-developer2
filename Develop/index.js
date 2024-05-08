b5cfa1d77f3bd41998a6daa372b81ef5f7c6b9aa
Clayton Andrews <claytonandrews897@gmail.com>
2024-05-08 15:11:12 -0400

    completed the readme developer

--- Develop/index.js
+++ Develop/index.js
@@ -1,10 +1,13 @@
-// TODO: Include packages needed for this application
-const fs = require("fs");
+// Packages needed for this application //
+//lcoal js in utils
+const createReadme = require('./utils/generateReadme');
+const getGithubFile = require("./utils/generateLicense");
+// External packages
 const inquirer = require("inquirer");
-const path = require("path");
-const generateMarkdown = require("./utils/generateMarkdown");
-
-// TODO: Create an array of questions for user input
+const async = require("async");
+//node native packages
+const fs = require('fs')
+//  Array of questions for user input
 const questions = [
     {
       type: "input",
@@ -12,22 +15,29 @@ const questions = [
       message: "Please name your Project.",
     },
     {
-      type: "input",
+      type: "editor",
       name: "description",
       message: "Please describe the purpose and functionality of this project.",
     },
+    
     {
-      type: "input",
-      name: "screenshot",
-      message: "Please provide the relative path to the image you want to use as the screenshot."
+      type: "editor",
+      name: "installation",
+      message: "Please provide the instructions to install your project"
     },
     {
       type: "input",
-      name: "link",
-      message: "Please provide a URL where a user can access your deployed application."
+      name: "usage",
+      message:
+        "State how you use the app",
     },
     {
-      type: "checkbox",
+      type: "editor",
+      name: "Tests",
+      message: "Please provide test instructions for your deployed application."
+    },
+    {
+      type: "list",
       name: "license",
       message: "Please select a license applicable to this project.",
       choices: ["MIT", "APACHE2.0", "Boost1.0", "MPL2.0", "BSD2", "BSD3", "none"],
@@ -42,12 +52,7 @@ const questions = [
       name: "features",
       message: "List some cool features about this project here.",
     },
-    {
-      type: "input",
-      name: "usage",
-      message:
-        "State the languages or technologies associated with this project.",
-    },
+
     {
       type: "input",
       name: "creator",
@@ -59,31 +64,79 @@ const questions = [
       message: "Provide a valid email address.",
     },
     {
-      type: "input",
+      type: "editor",
       name: "contributors",
       message: "Please list any contributors. (Use GitHub usernames)",
       default: "",
     },
     {
-      type: "input",
+      type: "editor",
       name: "test",
       message: "Provide walkthrough of required tests if applicable.",
+    },
+    {
+      type: 'list',
+      name: 'licenseDoc',
+      message: 'Do you want a license document? ',
+      choices: ['Yes', 'No']
     },
   ];
   
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {
-  return fs.writeFileSync(path.join(process.cwd(), fileName), data);
-}
+// The function to write README file
+const writeReadmeDoc = (fileName, data) => {
+    const text = createReadme(data);
+    fs.writeFile(filename, text, (err) =>
+      err ? console.error(err) : console.log('Commit logged')
+    );
 
-// TODO: Create a function to initialize app
-function init() {
-  inquirer.prompt(questions).then((inquirerResponses) => {
-    console.log('Generating README...');
-    writeToFile('README.md', generateMarkdown({ ...inquirerResponses }));
+const writeLicenseDoc = async (fileName, data) => {
+    const text =await getGithubFile(data.license);
+      fs.writeFile(fileName, text.data.body , (err) =>
+        err ? console.error(err) : console.log('License created')
+  }
+} ;
+// Interface
+cosnt printIntro = () => {
+
+  console.log(
+    #########################################################\n
+    Welcome to the README writer\n
+    #########################################################\n
+    How to use:\n
+    * Respond to each question with the appropriate information\n
+    * Some sections will pull up a text editor, put your desired information for that section, Close and click save\n
+    * Make sure the entered text is properly formatted with markdown to be rendered correctly\n
+    * You can learn more about markdown language here: https://www.markdownguide.org/basic-syntax/\n
+    * Leaving a question blank will still create the section but leave it blank\n
+    * writing '!DEL' will skip the section and it will not be created.\n
+    * If you need to exit early just "ctrl c"\n
+    #########################################################\n 
+  );
+};
+//INIT
+const init = () => {
+  printIntro();
+  letfolder = './Documents';
+  if (!fs.existSync(folder)) {
+    fs.mkdir(folder, (err) => {
+      if (err) {
+        console.log("failed to create folder");
+        return;
+      };
+    });
+  }
+  
+  //ask questions
+  inquirer.prompt(questions).then((response => {
+    //Writes readme
+    writeReadmeDoc(folder = '/README.md, reponse')
+    //Writes license file
+    if(response.licenseDoc === 'Yes'){
+      writeLicenseDoc(folder + '/License.md', respose);
+    }
   });
-}
+};
 
 // Function call to initialize app
 init();
---
+++ Develop/utils/generateLicense.js
@@ -0,0 +1,14 @@
+const { request } = require("octokit/request");
+
+const getGithubFile = license => {
+    license = license.toLowerCase().replace(/\)s/g, '-');
+    return new Promise((resolve) => {
+        resolve(
+            request('GET /licenses/${license}',{
+                license: 'license'
+            })
+        );
+
+    });
+};
+module.exports = getGithubFFile;
